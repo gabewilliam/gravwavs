72e427f5e7ceb2d956d8ec94212ae897cfbec436
stevet40 <stephen.thorp@hotmail.co.uk>
2017-02-10 00:14:21 +0000

    Add files via upload

--- 2DMCMC.cpp
+++ 2DMCMC.cpp
@@ -5,39 +5,84 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
-double likelihood(double x, double y, double sigma);
+
+//Prototype for the likelihood calculation function
+double likelihood(double x, double y, double a, double b, double s);
+double prior(double x, double y, double w);
+
 
 int main() {
 
+
+	//Initialises two random number generators
 	gsl_rng * normGen = gsl_rng_alloc(gsl_rng_taus);
 	gsl_rng * rGen = gsl_rng_alloc(gsl_rng_taus);
 	
 
-	std::cout<< "Enter the value of sigma:" << std::endl;
+	/*Takes an input for the standard deviation, sigma of the likelihood
+	/ distribution, and then calculates a value to use in the distribution
+	/ which is sampled to find the next x and y value [N(0,nSigma)]. The
+	/ distribution from which the random number is drawn is 1/10 the width of
+	/ the likelihood distribution. This is somewhat arbitrary, but means it
+	/ wont be too wide. A slightly wider distribution would maybe give faster
+	/ convergence.*/
+	std::cout << "Enter the standard deviation, sigma, of the likelihood:" 
+			  << std::endl;
 	double sigma;
 	std::cin >> sigma;
 
 	double nSigma = sigma/10;
 
+
+	//Takes inputs for the centre coordinates of the likelihood
+	std::cout << "Enter the values (a,b) the likelihood is centred on:" 
+			  << std::endl;
+	double a,b;
+	std::cout << "a:"<< std::endl;
+	std::cin >> a;
+	std::cout << "b:"<< std::endl;
+	std::cin >> b;
+
+
+	//Takes an input for the width of the prior's step function
+	std::cout<< "Enter the width of the prior:" << std::endl;
+	double w;
+	std::cin >> w;
+
+	
+	//Takes an input for the number of samples used in the Monte Carlo routine
 	std::cout<< "Enter the number of Monte-Carlo samples:" << std::endl;
-	double N;
+	int N;
 	std::cin >> N;
 
 
+	//Declares the variables used throughout the routine
 	double x, xPrime, y, yPrime, p, pPrime, alpha;
 	double nZeroX, nZeroY, rZero;
 
 
+	//Opens the output text file
 	FILE * outFile;
 	outFile = fopen("2DMonte.txt","w");
 
 
+	//Sets the starting point for the routine (pretty arbitrary)
 	x = 0;
 	y = 0;
 
+
+	/*Loops over the number of iterations speified by the input. In each run,
+	/ the likelihood function is evaluated at (x,y). Then, one of the RNGs is
+	/ used to draw values from a normal distribution of width nSigma. The
+	/ values obtained in this way are then used to find a new trial (x,y) pair
+	/ [(x',y') in Veitch's notation]. The likelihood at this new point is
+	/ then found. This is used to evaluate the acceptance value, alpha, which
+	/ is compared to a random number drawn from a uniform distribution, to
+	/ decide if (x',y') is accepted as the new (x,y). Either way, the (x,y)
+	/ is output to the file.*/
 	for(int i = 1; i <= N; i++) {
 
-		p = likelihood(x,y,sigma);
+		p = likelihood(x,y,a,b,sigma)*prior(x,y,w);
 
 
 		nZeroX = gsl_ran_gaussian(normGen, nSigma);
@@ -45,7 +90,7 @@ int main() {
 
 		xPrime = x + nZeroX;
 		yPrime = y + nZeroY;
-		pPrime = likelihood(xPrime,yPrime,sigma);
+		pPrime = likelihood(xPrime,yPrime,a,b,sigma)*prior(xPrime,yPrime,w);
 
 		alpha = pPrime/p;
 
@@ -62,6 +107,9 @@ int main() {
 
 	}
 
+
+	/*Closes the output file, then frees the memory associated with the random
+	/ number generators.*/
 	fclose(outFile);
 
 	gsl_rng_free(normGen);
@@ -70,8 +118,21 @@ int main() {
 
 }
 
-double likelihood(double x, double y, double sigma) {
 
-	return exp(-(x*x + y*y)/(2*sigma*sigma));
+/*Defines a likelihood function, which is a Gaussian of (x,y) centred on (a,b)
+/ and with a standard deviation of s.*/
+double likelihood(double x, double y, double a, double b, double s) {
+
+	return exp(-(pow((x-a),2) + pow((y-b),2))/(2*s*s));
+
+}
+
+
+/*Defines a prior function which is a step of height 1, centred on the origin,
+/ with a width of w.*/
+double prior(double x, double y, double w) {
+
+	if((std::abs(x) < (w/2)) && (std::abs(y) < (w/2))) { return 1; }
+	else { return 0; }
 
 }
