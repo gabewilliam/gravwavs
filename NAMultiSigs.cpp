18b64c42f065cf62eabcaca4f0b4bd0c63a7ab1c
izzierosho <isobel.romeroshaw@gmail.com>
2017-02-18 15:47:38 +0000

    Add files via upload

--- NAMultiSigs.cpp
+++ NAMultiSigs.cpp
@@ -14,15 +14,15 @@ using namespace std;
 
 typedef double twoVect[2];
 
-int C_PRECISION  = 17;
+#define C_PRECISION 17
+#define G_CONST 6.67E-11
+#define c_CONST 3.0E8
 
 double newtonianFrequency(double mA, double mB, double t){
-	double G = 6.67E-11;
-	double c = 3.0E8;
 	double A = 5.0/256.0;
 	double B = pow((mA + mB), (1.0/3.0));
-	double C = pow(c, 5.0);
-	double D = pow(G, (5.0/3.0));
+	double C = pow(c_CONST, 5.0);
+	double D = pow(G_CONST, (5.0/3.0));
 	double E = (A*B*C)/(mA*mB*t*D);
 	
 	double f = (1.0/M_PI)*pow(E, (3.0/8.0));
@@ -31,38 +31,31 @@ double newtonianFrequency(double mA, double mB, double t){
 	
 }
 
-double initialSeparation(double mA, double mB, double lowLimit){
-	double G = 6.67E-11;	
-	return pow((G*(mA + mB)/pow(2.0*M_PI*lowLimit, 2.0)), (1.0/3.0));
+double initialSeparation(double mA, double mB, double lowLimit){	
+	return pow((G_CONST*(mA + mB)/pow(2.0*M_PI*lowLimit, 2.0)), (1.0/3.0));
 }
 
 double waveAmplitude(double chirpM, double nextFreq, double lumD){
-	double G = 6.67E-11;
-	double c = 3.0E8;
-	double A = G/pow(c, 2.0);
+	double A = G_CONST/pow(c_CONST, 2.0);
     double B = (chirpM/lumD);
-	double C = G/pow(c, 3.0);
+	double C = G_CONST/pow(c_CONST, 3.0);
 	
 	return 4.0*A*B*pow(C*M_PI*nextFreq*chirpM, (2.0/3.0));
 }
 
 double chirpMagnitude(double chirpM, double nextFreq){
-	double G = 6.67E-11;
-	double c = 3.0E8;
 	double A = (96.0/5.0);
-	double B = pow(c, 3.0)/G;
+	double B = pow(c_CONST, 3.0)/G_CONST;
 	double C = nextFreq/chirpM;
 	
 	return A*B*C*pow(C*M_PI*nextFreq*chirpM, (8.0/3.0));
 }
 
 double timeTilMerge(double mA, double mB, double sep){
-	double G = 6.67E-11;
-	double c = 3.0E8;
 	double A = 5.0/256.0;
 	double B = pow(sep, 4.0);
-	double C = pow(c, 5.0);
-	double D = pow(G, 3.0);
+	double C = pow(c_CONST, 5.0);
+	double D = pow(G_CONST, 3.0);
 	double E = mA*mB*(mA + mB);
 	
     return (A*B*C)/(D*E);
@@ -134,8 +127,10 @@ int main(){
 	// Initialise masses
 	double maxM = 200.0;
 	double M1, M2;	
-	// Set luminosity distance in MPc
-	double lumDist = 500.0;	
+	// Randomly set luminosity distance in MPc
+	// NEED TO MAKE THIS RANGE REALISTIC
+	double maxDist = 1000.0;
+	double lumDist = double(rand())*maxDist/double(RAND_MAX);	
 	// Set lower sensitivity of LIGO band
 	double lowBound = 20.0;
 	
