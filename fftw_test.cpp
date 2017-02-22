c75a5803fecc50bae2bb3996ecbef9259e68512a
Eli Chadwick <eli.chadwick.256@gmail.com>
2017-02-22 22:45:01 +0000

    Working FFTW code

--- fftw_test.cpp
+++ fftw_test.cpp
@@ -3,68 +3,46 @@
 #include <iostream>
 #include <vector>
 #include <fftw3.h>
-#include "ReadandPrint.h"//print function
-#include "templates.h" //for Template struct
+#include "gwReadWrite.h"//print function
+#include "gwDataTypes.h" //for Template struct
 
 #define REAL(z, i) (z[i][0])
 #define IMAG(z, i) (z[i][1])
 
 int main(){
 
-	//Setting up input stream for signal
-	std::ifstream in_file;
-	in_file.open("signalB.dat");
-	
-	//Checking if stream is setup succesfully
-	if(in_file.fail())
-	{
-		std::cout << "Input file not found" << std::endl;
+	//import signals
+	std::vector<Signal> signals;
+
+	if (!loadSignals("NonAdaptiveSignal.dat",&signals,csv)){
+		std::cout<<"Signal file not found"<<std::endl;
 		return 0;
 	}
-	
-	//transfer signal to vector
-	std::vector<double> signalTime;
-	std::vector<double> inputSignal;
 
-	double a,b;
-
-	while (!in_file.eof()){	
-
-		in_file>>a>>b;
-		signalTime.push_back(a);
-		inputSignal.push_back(b);
-	}
-
+	std::cout<<"Successfully imported signal."<<std::endl;
 	//should loop through all templates and set up to use this
-	std::vector<std::vector<double> > printer;
-	printer.push_back(signalTime);
-	int N=inputSignal.size();
+	std::vector< Signal > printer;
+	
+	//put within loop
+	int N=signals[0].waveform[1].size();
 
 	//import filters
-	std::vector	<double> filterTime;
-	std::vector<Template>filters;
+	std::vector< Template > filters;
 
 	//load templates
-	if (!load_templates("templates2.csv",&filters,csv)){
+	if (!loadTemplates("NonAdaptiveTemplates.dat",&filters,csv)){
 		std::cout<<"Template file not found"<<std::endl;
 		return 0;
 	}
 
-	for(size_t j=0;j<filters.size();++j){
-		while (filters[j].waveform.size()<inputSignal.size()){
-			filters[j].waveform.push_back(0.0);
-		}
-	}
+	std::cout<<"Successfully imported filters."<<std::endl;
 
 	//create output - currently messy
-	std::vector<double> output(N);
-	std::vector<double> outputB(2*N-1);
+	vec_d output(N);	
+	vec_d outputB(2*N-1);
 
-	//single filter - set up to loop over all
-	std::vector<double> inputFilter(N);
-	for (int i=0; i<N; i++) {
-		inputFilter[i]=filters[0].waveform[i];
-	}
+	//single filter
+	vec_d inputFilter(N);
 
 	//create complex arrays for use in transforms
 	fftw_complex * signal, * filter, * signalOutput, * filterOutput, * product, * convolution;
@@ -80,48 +58,73 @@ int main(){
 
 	//create plans for each transform
 	//plans can be re-used but I need to figure out how
-	fftw_plan planSig=fftw_plan_dft_1d(2*N-1,signal,signalOutput,FFTW_FORWARD,FFTW_MEASURE);
-	fftw_plan planFil=fftw_plan_dft_1d(2*N-1,filter,filterOutput,FFTW_FORWARD,FFTW_MEASURE);
-	fftw_plan planCon=fftw_plan_dft_1d(2*N-1,product,convolution,FFTW_BACKWARD,FFTW_MEASURE);
 
-	//set real and imaginary parts of all elements of signal and filter
+	fftw_plan planSig=fftw_plan_dft_1d(2*N-1,signal,signalOutput,FFTW_FORWARD,FFTW_ESTIMATE);
+	fftw_plan planFil=fftw_plan_dft_1d(2*N-1,filter,filterOutput,FFTW_FORWARD,FFTW_ESTIMATE);
+	fftw_plan planCon=fftw_plan_dft_1d(2*N-1,product,convolution,FFTW_BACKWARD,FFTW_ESTIMATE);
+
+	//set signal
 	for (int i=0; i<2*N-1; i++){
-		if (i<N) {
-			signal[i][0]= inputSignal[i];
-			filter[i][0] = inputFilter[i];
+		if (i<N){
+			signal[i][0]=signals[0].waveform[1][i];
 		}
 		else {
 			signal[i][0]=0.0;
-			filter[i][0]=0.0;
 		}
 		signal[i][1]=0.0;
-		filter[i][1]=0.0;
 	}
-
-	//carry out forward transforms
+	
+	//forward transform of signal
 	fftw_execute(planSig);
-	fftw_execute(planFil);
-	
-	//calculate product of real and imaginary parts
-	for (int i=0; i < 2*N-1; i++) {
-		product[i][0]=signalOutput[i][0]*filterOutput[i][0];
-		product[i][1]=-signalOutput[i][1]*filterOutput[i][1];
-	}
-	
-	//carry out reverse transform
-	fftw_execute(planCon);
-	
-	//set up output - needs fixing so length is back to N if possible - will look into
-	for (int i=0; i<2*N-1; i++) {
-		outputB[i]=convolution[(i+N)%(2*N-1)][0];
-	}
 
-	//print - should use printer but it segfaulted once and I'm lazy
-	FILE * fileout;
-	fileout=fopen("testout2.csv","w");
-	for (int i=0; i<N; i++){
-		//real part used for now because it looks nice
-		fprintf(fileout,"%g, ",convolution[i][0]/*sqrt(pow(convolution[i][0],2)+pow(convolution[i][1],2))*/);
+	for(size_t j=9;j<10;++j){
+		while (filters[j].waveform[1].size()<N){
+			filters[j].waveform[1].push_back(0.0);
+		}
+		for (int i=0; i<N; i++) {
+			inputFilter[i]=filters[0].waveform[1][i];
+		}
+		//set real and imaginary parts of all elements of signal and filter
+		for (int i=0; i<2*N-1; i++){
+			if (i<N) {
+				filter[i][0] = inputFilter[i];
+			}
+			else {
+				filter[i][0]=0.0;
+			}
+			filter[i][1]=0.0;
+		}
+		//forward transform of filter
+		fftw_execute(planFil);
+	
+		//calculate product of real and imaginary parts
+		for (int i=0; i < 2*N-1; i++) {
+			product[i][0]=signalOutput[i][0]*filterOutput[i][0];
+			product[i][1]=-signalOutput[i][1]*filterOutput[i][1];
+		}
+	
+		//carry out reverse transform
+		fftw_execute(planCon);
+	
+		//set up output - needs fixing so length is back to N if possible - will look into
+		for (int i=0; i<2*N-1; i++) {
+			outputB[i]=convolution[i][0];
+		}
+		//   convolution[i][0]   sqrt(pow(convolution[i][0],2)+pow(convolution[i][1],2))
+		//    sqrt(pow(convolution[(i+N)%(2*N-1)][0],2)+pow(convolution[(i+N)%(2*N-1)][1],2))
+
+		Signal output;
+		for (int i=0; i<2*N-1;i++){
+			output.waveform[0].push_back(signals[0].waveform[0][i]);
+			output.waveform[1].push_back(outputB[i]);
+		}
+		printer.push_back(output);	
+		std::cout<<j<<std::endl;
+	}
+	
+	if(!saveSignals("testout5.csv",&printer,csv)){
+		std::cout<<"Save failed."<<std::endl;
+		return 0;
 	}
 
 	//free memory
