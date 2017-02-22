5d941353d08b2a1f999716d8ccb68c07abd334dd
SJT459 <sjt459@phymat4.adf.bham.ac.uk>
2017-02-22 16:15:19 +0000

    Updated version of MATLAB script, outputting m1 and m2

--- TwoDMCMCPlotter.m
+++ TwoDMCMCPlotter.m
@@ -1,58 +1,53 @@
 %Loads the data from the text file into the workspace
 fid=fopen('2DMonte.txt');
 
-constants = fscanf(fid,'%g,%g,%g',3);
+%Extracts the data from the file. The m1 values are stored in X. The m2
+%values are stored in Y. This convention is followed throughout.
+constants = fscanf(fid,'%g',1);
 XY = fscanf(fid,'%g,%g\n',[2 Inf]);
 XY = XY';
 X = XY(:,1);
 Y = XY(:,2);
 
-s = constants(1);
-a = constants(2);
-b = constants(3);
-
-%Creates circles of radius 1sigma and 2sigma, for illustrative purposes
-x12 = linspace(-s+a,s+a,10000);
-y1 = b + sqrt(s^2 - (x12-a).^2);
-y2 = b - sqrt(s^2 - (x12-a).^2);
-x34 = linspace(-2*s+a,2*s+a,10000);
-y3 = b + sqrt(4*s^2 - (x34-a).^2);
-y4 = b - sqrt(4*s^2 - (x34-a).^2);
-
-%Makes a figure featuring an (x,y) scatter and a histogram.
-%Also now features a nice colourmap, and rings showing 1 and 2 sigma.
+%Makes a figure featuring an (m1,m2) scatter and a histogram.
+%Also now features a nice colourmap.
 %It only really looks good when you maximise the plot window.
+%This first block makes the scatter.
 subplot(2,2,1)
-samp = scatter(X,Y,'.')
-xlabel('x')
-ylabel('y')
+samp = scatter(X,Y,'.');
+xlabel('m_1/kg')
+ylabel('m_2/kg')
 title('Data Point Plot')
 hold on
-c1 = plot(x12,y1,'r')
-plot(x12,y2,'r')
-c2 = plot(x34,y3,'g')
-plot(x34,y4,'g')
-legend([samp,c1,c2],{'(x,y) Samples','1\sigma Contour','2\sigma Contour'})
+legend([samp],{'(m_1,m_2) Samples'})
 %daspect([1 1 1])
 hold off;
 
+%This makes the histogram.
 subplot(2,2,[2,4])
 hist3(XY,[20 20])
-xlabel('x')
-ylabel('y')
+xlabel('m_1/kg')
+ylabel('m_2/kg')
 zlabel('Sample Density')
 title('Posterior Histogram')
 set(get(gca,'child'),'FaceColor','interp','CDataMode','auto')
 
+%This extracts some data from the histogram.
 subplot(2,2,3)
 [n,C] = hist3(XY,[20 20]);
-[M,I] = max(n)
-[N,Iy] = max(M)
-Ix = I(Iy)
+[M,I] = max(n);
+[N,Iy] = max(M);
+Ix = I(Iy);
 
-xMax = C{1}(Ix)
-yMax = C{2}(Iy)
+%Finds the highest histogram bar and the coresponding (m1,m2) combination.
+xMax = C{1}(Ix);
+yMax = C{2}(Iy);
+m1Max = sprintf('The best value of m_1 is %e kg',xMax);
+m2Max = sprintf('The best value of m_2 is %e kg',yMax);
+disp(m1Max);
+disp(m2Max);
 
+%This plots the density of the histogram as a flat colourmap.
 n1 = n';
 n1(size(n,1) + 1, size(n,2) + 1) = 0;
 xb = linspace(min(X),max(X),size(n,1)+1);
@@ -61,13 +56,10 @@ h = pcolor(xb,yb,n1);
 h.ZData = ones(size(n1)) * -max(max(n));
 colormap(hsv)
 hold on
-plot(x12,y1,'k')
-plot(x12,y2,'k')
-plot(x34,y3,'k')
-plot(x34,y4,'k')
-xlabel('x')
-ylabel('y')
+xlabel('m_1/kg')
+ylabel('m_2/kg')
 title('Histogram Density Plot')
 
-fclose(fid)
+fclose(fid);
+
 %Some of the code is lifted from the MATLAB documentation on hist3
