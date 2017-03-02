bc7bab78fd51811b22e4aedff2941be14a98f6db
dxp462 <dxp462@student.bham.ac.uk>
2017-03-02 13:47:37 +0000

    Update Binary.cpp

--- Binary.cpp
+++ Binary.cpp
@@ -1,5 +1,6 @@
 //Write functions for Binary.h in here
 #include "binary.h";
+#include <math.h>;
 
 
 //GET METHODS
@@ -44,10 +45,22 @@ double Binary::getSeparation (){
   
 }
 
-double Binary::getRatio (){
+double Binary::getRatio (int n){
   
-  double q=fm2/fm1;//assume you want the ratio m2:m1 so that it's <1 but idk
-  return q;
+  if (n==1){
+    double q=fm1/fm2;
+    return q;
+  }
+  
+  else if (n==2){
+    double q=fm2/fm1;
+    return q;
+  }
+  
+  else {
+    std::cout<<"you fucked it"<<std::endl;
+    return 0;
+  }
   
 }
 
@@ -72,9 +85,30 @@ void Binary::setMass (int n, double m){
 
 }
 
-void Binary::setSeparation (double ){
+void Binary::setSeparation (double a){
   
   fa=a;
   return;  
 
 }
+
+
+//COMPUTING BINARY VARIABLES
+
+void Binary::updateRadii (){
+  
+  fr1=pow(fm1,0.6);
+  fr2=pow(fm2,0.6);
+  
+}
+
+double Binary::rocheLobe(int n){
+  
+  double q = this->getRatio(n);
+  double qonethird = pow(q,1/3);
+  double qtwothird = pow(q,2/3);
+  double rl = 0.49*qtwothird/((0.6*qtwothird)+log(1+qonethird));
+  return rl;
+  
+}
+  
