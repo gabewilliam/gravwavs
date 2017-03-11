333d964e7d9984c028fa612989f7c95c37b0fa5c
SJT459 <sjt459@phymat4.adf.bham.ac.uk>
2017-03-11 00:45:51 +0000

    Calculates the merger rate badly

--- StellarPopV6.cpp
+++ StellarPopV6.cpp
@@ -9,19 +9,34 @@
 
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_histogram.h>
+#include <gsl/gsl_errno.h>
+
+
+double CDF(double Z, double z);
+double dMSFRdtdV(double z);
+double Ez(double z);
+double tIntegrand(double z, void * params);
+double tLookback(double z);
+double birthRate(double Z, double z, double M);
 
 
 int main() {
 
 	
 	//Defines a few key constants in SI units
-	double mSolar = 1.989e30; //kg
-	double rSolar = 6.975e8; //m
-	double AU = 1.496e11; //m
-	double c = 3.0e8; //ms^-2
-	double G = 6.67408e-11;//m^3 kg^-1 s^-2
-	double yr = 365.25*24*60*60;//s
+	const double mSolar = 1.989e30; //kg
+	const double rSolar = 6.975e8; //m
+	const double AU = 1.496e11; //m
+	const double c = 3.0e8; //ms^-2
+	const double G = 6.67408e-11;//m^3 kg^-1 s^-2
+	const double yr = 365.25*24*60*60;//s
+	
 
+
+	//GENERATING A POPULATION OF BINARIES
 
 	/*Sets up a random number generator to seed the other two generators, and
 	/ seeds this based on the system time.*/
@@ -203,5 +218,171 @@ int main() {
 	gsl_rng_free(qGen);
 	gsl_rng_free(aGen);
 
+
+
+	//FINDING THE MERGER RATE
+
+	gsl_set_error_handler_off();//UNLIMITED POWER
+	
+	//Defines the maximum redshift which will be looked at
+	double zMax = 25;
+
+	//Defines the number of bins the universe is to be split into
+	size_t nBins = 10;
+
+	gsl_histogram * zHist = gsl_histogram_alloc(nBins);
+	gsl_histogram_set_ranges_uniform(zHist,0,zMax);
+	gsl_histogram * tHist = gsl_histogram_alloc(nBins);
+	
+	double tRanges[nBins+1];
+	tRanges[0] = 0;
+
+	double zLower, zUpper, tUpper;
+	
+	for(int j = 0; j < nBins; j++) {
+
+		gsl_histogram_get_range(zHist, j, &zLower, &zUpper);
+		tUpper = tLookback(zUpper);
+		tRanges[j+1] = tUpper;
+
+	}
+
+	std::cout << tRanges[nBins] <<std::endl;
+
+	gsl_histogram_set_ranges(tHist, tRanges, nBins+1);
+
+	double tjMax, tjMin, tj, tiMax, tiMin, ti,
+			zjMax, zjMin, zj, ziMax, ziMin, zi;
+	double dtj, dti;
+	size_t mergeIndexMax;
+	size_t mergeIndexMin;
+	double dNMergedtdV;
+
+	double Zmax = 0.004;
+	
+	FILE * rateFile;
+	rateFile = fopen("rates.csv","w");
+
+	//Merge time bins
+	for(int j = 0; j < nBins; j++) {
+
+		dNMergedtdV = 0;
+
+		gsl_histogram_get_range(zHist, j, &zjMin, &zjMax);
+		zj = (zjMax +zjMin)/2;
+		gsl_histogram_get_range(tHist, j, &tjMin, &tjMax);
+		dtj = tjMax - tjMin;
+	
+		//Binaries
+		for(int k = 0; k < nCandidates; k++) {
+		
+			tm = binaries[k].mergeTime()/yr;
+
+			//Birth time bins
+			for(int i = 0; i < nBins; i++) {
+
+				//std::cout<<j<<" "<<k<<" "<<i<<std::endl;
+
+				gsl_histogram_get_range(zHist, i, &ziMin, &ziMax);
+				zi = (ziMax +ziMin)/2;
+				ti = tLookback(zi);
+
+				gsl_histogram_get_range(tHist, i, &tiMin, &tiMax);
+				dti = tiMax - tiMin;
+				tiMin += tm;
+				tiMax += tm;
+		
+				//std::cout<<tiMax<<std::endl;	
+				gsl_histogram_find(tHist, tiMin, &mergeIndexMin);
+				gsl_histogram_find(tHist, tiMax, &mergeIndexMax);
+
+				if((mergeIndexMin == j) || (mergeIndexMax == j)) {
+					dNMergedtdV += birthRate(Zmax, zj, mTotal)*(dti/dtj);
+				}
+											
+			}
+			
+		}
+
+		fprintf(rateFile, "%.15g,%.15g\n", zj, dNMergedtdV);
+
+	}
+
+	fclose(rateFile);
+
+	gsl_histogram_free(zHist);
+	gsl_histogram_free(tHist);
+
+}
+
+
+double CDF(double Z, double z) {
+
+	const double ZSolar = 0.0134;
+	
+	double alpha = -1.16;
+	double beta = 2;
+	
+	double a = alpha + 2;
+	double Zbeta = pow((Z/ZSolar),beta);
+	double tenbetaz = pow(10,(0.15*beta*z));
+	double x = Zbeta*tenbetaz;
+
+	return gsl_sf_gamma_inc_P(a,x);
+
+}
+
+double dMSFRdtdV(double z) {
+
+	double num = pow((1+z),2.7);
+	double denom = pow((1+(1+z)/2.9),5.6);
+	
+	return 0.015*num/denom;
+	
+}
+
+double Ez(double z) {
+
+	const double omegaLambda = 0.718;
+	const double omegaM = 1 - omegaLambda;
+
+	double Esquared = omegaM*pow((1+z),3) +omegaLambda;
+	return sqrt(Esquared);
+
+}
+
+double tIntegrand(double z, void * params) {
+
+	return 1/(Ez(z)*(1+z));
+
+}
+
+double tLookback(double z) {
+
+	double tH =  4.55e17;//s
+	const double yr = 365.25*24*60*60;//s
+	tH = tH/yr; //Converts the Hubble time to years
+
+	gsl_integration_workspace * w = gsl_integration_workspace_alloc(1000);
+	
+	double tL, tError;
+	double param = 1; //I think this has to be done :(
+	double epsRel = 1e-5;
+
+	gsl_function tInt;
+	tInt.function = &tIntegrand;
+	tInt.params = &param;
+
+	gsl_integration_qag(&tInt, 0, z, 0, epsRel, 1000, 6, w, &tL, &tError);
+
+	gsl_integration_workspace_free(w);
+
+	return tH*tL;
+
+}
+
+double birthRate(double Z, double z, double M) {
+
+	return (CDF(Z,z)*dMSFRdtdV(z))/M;
 
 }
