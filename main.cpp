c4f9de99458fd792c48c2700c15700d548685bd8
Eli Chadwick <eli.chadwick.256@gmail.com>
2017-03-12 00:19:25 +0000

    hopefully not a memory colander any more

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main()
 	std::vector<Signal> sigs;
 	std::vector<Template> temps;
 	
-	if(!loadSignals("../data/sig_30_26.csv", &sigs, csv))
+	if(!loadSignals("../data/SpecialNoiseSignalForEverybody.csv", &sigs, csv))
 		return 0;
 
-	if(!loadTemplates("../data/temp_bank.csv", &temps, csv))
+	if(!loadTemplates("../data/temp_50_30.csv", &temps, csv))
 		return 0;
 
 	int N, n;
@@ -32,26 +32,49 @@ int main()
 
 	Extractor bill;
 	
-	bill.setSignalF(&sig);
+	if(bill.loadCurve("../data/noisecurve.csv")){
+		std::cout<<"noise successfully loaded"<<std::endl;
+	}else{
+		std::cout<<"no noise I'm afraid"<<std::endl;
+		return 0;
+	}
+	
+	bill.setSignalT(&sig);
 	bill.setTemplates(&temps);
+	
+	std::cout << "splitting" << std::endl;
+	
+	bill.splitSignal();
+	
+	std::cout << "FFT" << std::endl;
+	
+	bill.fft();
 
 	std::cout << "Performing testuency domain convolution against all templates..." << std::endl;
 
 	std::vector<Template> conOut;
 
-	bill.Convolution(&conOut);
+	bill.Convolution();
 
 	saveTemplates("../data/conout.dat", &conOut, csv);
 
 	std::cout << "Performing inverse fourier transform" << std::endl;
 	
-	std::vector<Template> output;
+	std::vector<Template> output1;
+	std::vector<Template> output2;
 
-	bill.fftInverse(&output);
+	bill.fftInverse(&output1);
+	std::cout<<"doe breah"<<std::endl;
+	std::cout<<output1[0].waveform[0][0]<<std::endl;
 	
 	std::cout << "Saving output to output.dat..." << std::endl;
 	
-	saveTemplates("../data/output.dat", &output, csv);
+	saveTemplates("../data/output1.dat", &output1, csv);
+	//saveTemplates("../data/output2.dat", output2, csv);
+
+	bill.clearOtherMemory();
+
+	std::cout<<"Complete."<<std::endl;
 
 	return 1;
 }
