1f2354326c9bb4a59831db29b402b3421cda7f52
DXR405 <dxr405@phymat4.adf.bham.ac.uk>
2017-03-20 22:30:51 +0000

    chris smells very bad and not with his nose

--- parameterExtractionPloter.m
+++ parameterExtractionPloter.m
@@ -1,5 +1,5 @@
 %Loads the data from the text file into the workspace
-fid=fopen('RawData.txt');
+fid=fopen('RawData500k.txt');
 
 %Extracts the data from the file.
 dataFile = fscanf(fid,'%g,%g,%g\n',[3 Inf]);
@@ -7,6 +7,26 @@ dataFile = dataFile';
 mRatio = dataFile(:,1);
 mChirp = dataFile(:,2);
 distance = dataFile(:,3);
+
+%calculate credible intervals for variables
+
+%size of credible interval (e.g. 0.90 = 90%)
+intervalSize = 0.90;
+
+%assumes same no of datapoints for all parameters
+numDatapoints = size(dataFile,1);
+
+%sort file of each parameter and find the values at interval boundaries
+sortFile = sort(dataFile);
+
+ratioLower = sortFile(int32(((1-intervalSize)/2)*numDatapoints),1);
+ratioUpper = sortFile(int32(((1+intervalSize)/2)*numDatapoints),1);
+
+chirpLower = sortFile(int32(((1-intervalSize)/2)*numDatapoints),2);
+chirpUpper = sortFile(int32(((1+intervalSize)/2)*numDatapoints),2);
+
+distLower = sortFile(int32(((1-intervalSize)/2)*numDatapoints),3);
+distUpper = sortFile(int32(((1+intervalSize)/2)*numDatapoints),3);
 
 %Plots various pairings of variables
 % plot(mRatio)
@@ -19,4 +39,19 @@ distance = dataFile(:,3);
 % plotParams( distance, 'distance', 'distance / m', mRatio, 'mass ratio', 'mass ratio');
 ma = cM*(1+mR)^0.2*mR^-0.6
 mb = cM*(1+mR)^0.2*mR^0.4
+
+for n=1:numDatapoints
+    currentChirp = mChirp(n);
+    currentRatio = mRatio(n);
+    massList(n,1) = currentChirp*(1+currentRatio)^0.2*currentRatio^-0.6;
+    massList(n,2) = currentChirp*(1+currentRatio)^0.2*currentRatio^0.4;
+end
+massListSort = sort(massList);
+
+maLower = massListSort(int32(((1-intervalSize)/2)*numDatapoints),1);
+maUpper = massListSort(int32(((1+intervalSize)/2)*numDatapoints),1);
+
+mbLower = massListSort(int32(((1-intervalSize)/2)*numDatapoints),2);
+mbUpper = massListSort(int32(((1+intervalSize)/2)*numDatapoints),2);
+
 fclose(fid);
