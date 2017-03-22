c21e8a5f0d226c7b684fe77c93f0cc1704512537
SJT459 <sjt459@phymat4.adf.bham.ac.uk>
2017-03-22 17:06:16 +0000

    Whatever

---
+++ BinaryFucked.h
@@ -0,0 +1,55 @@
+
+
+class Binary {
+
+	public:
+
+		Binary();
+		
+		Binary(double mass1, double mass2, double separation);
+
+		virtual ~Binary();
+
+		double getMass(int n);
+		double getRadius(int n);
+		double getSeparation();
+		double getRatio(int n);//input decides which mass is on top of fraction
+		double gettMerge();
+		
+		void printGets();//couts all the gets (above)
+
+		void setMass(int n, double m);
+		void setSeparation(double a);
+
+		void updateRadii();
+
+		double radius(int n);
+		double rocheLobe(int n);
+		double mixingRatio(int n);
+		double omegaC(int n);
+		double mergeTime();
+
+		void evolveMainSequence();
+		void evolveWolfRayet();
+		void evolveSupernova();
+
+		bool checkMassRange();
+		bool checkRocheLobe();
+		bool checkHomogeneousMixing();
+		bool checkBreakup();
+		bool checkPairInstability();
+		bool checkMergeTime();
+		int checkCandidate();
+		bool steveIsATotalLoserAndDeservesToDie();
+			
+
+	private:
+		
+		double fm1;
+		double fm2;
+		double fr1;
+		double fr2;
+		double fa;	//atm SPV5 assigns this in AU
+		double fTMerge;	
+		
+};
