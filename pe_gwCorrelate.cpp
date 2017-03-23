73f1cd22617a4e118c7b35796b1e4eba6e146551
chrisburch96 <chris_burch@hotmail.co.uk>
2017-03-23 20:58:41 +0000

    Correlate

--- pe_gwCorrelate.cpp
+++ pe_gwCorrelate.cpp
@@ -8,14 +8,16 @@
 #include <vector>
 #include <gsl/gsl_statistics_double.h>
 
+#include "pe_gwSaveToFile.h"
+
 double autoCorrelation(double [], int);
-double checkForNaN(double);
-void saveToFile(double [],double [],double [],int,int,std::string);
 
 int main(){
 	
+	//INSERT PARAMETER FILE NAME HERE
 	std::string fileName = "RawData.txt";
 	
+	//Finds number of rows in file
 	std::ifstream lineFile;
 	lineFile.open( fileName.c_str() );
 
@@ -28,31 +30,38 @@ int main(){
 		++N;
 	}
 
+	//Opens file to be read to arrays
 	FILE * inFile;
 	inFile=fopen( fileName.c_str() ,"r" );
 	
+	//Initialises parameter arrays
 	double * chirpMass = new double[N];
 	double * massRatio = new double[N];
 	double * distance = new double[N];
 
+	//Reads raw data file into parameter arrays
 	for( int i = 0; i < N; i++ ){
 		fscanf( inFile, "%lf %*c %lf %*c %lf\n", &chirpMass[i], &massRatio[i], &distance[i] );
 	}
 
-	double corrFactorChirp = checkForNaN( autoCorrelation( chirpMass, N ) );
-	double corrFactorRatio = checkForNaN( autoCorrelation( massRatio, N ) );
-	double corrFactorDistance = checkForNaN( autoCorrelation( distance, N ) );
+	//Calculates autocorrelation for each parameter
+	double corrFactorChirp = autoCorrelation( chirpMass, N );
+	double corrFactorRatio = autoCorrelation( massRatio, N );
+	double corrFactorDistance = autoCorrelation( distance, N );
 	
+	//Prints ACL values
 	std::cout << "Chirp Mass: " << corrFactorChirp << std::endl;
 	std::cout << "Mass Ratio: " << corrFactorRatio << std::endl;
 	std::cout << "Distance: " << corrFactorDistance << std::endl;
 
-	
+	//Picks the largest ACL value
 	double acls [] = {corrFactorChirp, corrFactorRatio, corrFactorDistance};
 	int aclMax = *(std::max_element(acls,acls+3));
 	
-	saveToFile( chirpMass, massRatio, distance, aclMax, N, "DataUncorr.txt" );
+	//Saves parameters to a file using the maximum ACL value as a thinning factor
+	saveArraysToFile( chirpMass, massRatio, distance, aclMax, N, "ThinnedData.txt" );
 
+	//Deallocates memory
 	delete [] chirpMass;
 	delete [] massRatio;	
 	delete [] distance;
@@ -63,53 +72,31 @@ int main(){
 
 double autoCorrelation( double inArray[], int size ){
 
+	//Initialises variables
 	int lag = 1;
 	double acl = 0;
 	double autoCorr=1;
 
+	//Keeps looping until the autocorrelation is no longer positive
 	while(autoCorr==std::abs(autoCorr)) {
 
+		//Redefines arrays (the arrays decrease in size each iteration so have to be redeclared)
 		double * lagArray = new double[size-lag];
 		double * leadArray = new double[size-lag];
+
+		//Removes elements from the end of one array and from the start of the other then cross-correlates them
 		std::copy(inArray+lag,inArray+size,leadArray);
 		std::copy(inArray,inArray+(size-lag),lagArray);
 		autoCorr=gsl_stats_correlation(lagArray, 1, leadArray, 1, (size-lag));
-		if(!(autoCorr==autoCorr)){
-			for(int i=0;i<size;i++){
-				std::cout<<inArray[i]<<std::endl;
-			}
-		}
-		if(lag%100==0) std::cout<<"Lag = "<<lag<<std::endl;
 
+		//Increments the lag and calculates the ACL
 		lag+=1;
 		acl+=2*autoCorr;
 
+		//Deallocates memory
 		delete [] lagArray;
 		delete [] leadArray;
 	}
 
 	return acl;
 }
-
-void saveToFile(double parameterA[], double parameterB[], double parameterC[], int lag, int size, std::string fileName) {
-	
-	//Opens the output text file
-	FILE * outFile;
-	outFile = fopen(fileName.c_str(),"w");
-
-	for(int i = 0; i < size; i++){
-		if (i%lag==0 && i!=0){
-			fprintf(outFile,"%.15g,%.15g,%.15g\n",parameterA[i],parameterB[i],parameterC[i]);
-		}
-	}
-	
-	//Closes the output file
-	fclose(outFile);
-}
-
-double checkForNaN(double ACL) {
-	if(!(ACL==ACL)) return 1;
-	else return ACL;
-}
-
-
